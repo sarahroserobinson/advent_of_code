bb3a2ae03ed9d7e1056e4cd2cc3b59ff048ac56d
Sarah Robinson <sarahrose@Sarahs-MacBook-Pro-8.local>
2024-12-08 22:59:32 +0000

    add math function and solution

--- 03_Mull_It_Over/solution.py
+++ 03_Mull_It_Over/solution.py
@@ -1,4 +1,3 @@
-# correct format is mul(x, y)
 import re
 
 def get_input():
@@ -11,9 +10,19 @@ data = get_input()
 def find_uncorrupted_muls(data):
     accepted_mul = []
     matches = re.findall(r"mul\(\d{1,3},\d{1,3}\)", data)
-    print(matches)
     accepted_mul.extend(matches)
     return accepted_mul
 
-print(find_uncorrupted_muls(data))
+def do_math(uncorrupted_muls):
+    count = 0
+    for item in uncorrupted_muls:
+        nums_as_str = item[4:-1].split(',')
+        a = int(nums_as_str[0])
+        b = int(nums_as_str[1])
+        count += a * b
+    return count   
+
+uncorrupted_muls = find_uncorrupted_muls(data)
+answer = print(do_math(uncorrupted_muls))
+
     
