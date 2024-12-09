3d41882533a655e7e31ae24aabd6376219b3927a
Sarah Robinson <sarahrose@Sarahs-MacBook-Pro-8.local>
2024-12-09 15:28:12 +0000

    change way of making word search and adds function to display word search

--- 04_Ceres_Search/solution.py
+++ 04_Ceres_Search/solution.py
@@ -1,14 +1,19 @@
 def get_input():
     with open('input.txt', 'r') as file:
-        input = file.read().split()
+        input = file.read().split('\n')
         return input
     
 def make_word_search(input):
     word_search = []
     for line in input:
-        word_search += list(line)
-    print(word_search)
+        word_search.append(list(line))
+    return word_search
     
-
+def display_word_search(word_search):
+    for line in word_search:
+        print(" ".join(line))
+    
 input = get_input()
-make_word_search(input)
+word_search = make_word_search(input)
+display_word_search(word_search)
+
